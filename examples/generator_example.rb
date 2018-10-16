0cab0061d212f4f94631f366aa6ae6c1af428ec7
Dmitry <avalonsdm@gmail.com>
2018-10-16 17:12:15 +0300

    change generator_example

--- examples/generator_example.rb
+++ examples/generator_example.rb
@@ -3,11 +3,11 @@ require 'reportage_dsl'
 # V1
 #
 # Generate PDF from string
-ReportageDSL::PDFGenerator.generate('Hello World')
+ReportageDSL::PDFGenerator.generate('Hello World', file: './file.pdf')
 # Generate PDF from data array
-ReportageDSL::PDFGenerator.generate(['Hello World', 'Goodbye World'])
+ReportageDSL::PDFGenerator.generate(['Hello World', 'Goodbye World'], file: './file.pdf')
 # Generate XLSX from string
-ReportageDSL::XLSXGenerator.generate('Hello World')
+ReportageDSL::XLSXGenerator.generate('Hello World', file: './file.xlsx')
 
 # V2
 #
@@ -17,3 +17,15 @@ ReportageDSL::Generator.generate('Hello World', format: :pdf)
 ReportageDSL::Generator.generate(['Hello World', 'Goodbye World'], header: ['col1', 'col2'], format: :pdf)
 # Generate XLSX from string
 ReportageDSL::Generator.generate('Hello World', header: ['col1', 'col2'], format: :xlsx)
+
+# V3
+#
+# Generate PDF
+rep = ReportageDSL::Generator.new
+rep.add [0, 1, 2]
+rep.to_pdf
+
+#Generate XML
+rep = ReportageDSL::Generator.new
+rep << [0, 1, 2]
+rep.to_xml
